61912d215b7713e055fb28dba92021038060d5f2
Georgii Kostiuchik <georgii.kostiuchik@gmail.com>
2023-08-19 15:55:02 +0200

    fix set resize

--- docs/stratification.js
+++ docs/stratification.js
@@ -1,50 +1,43 @@
 import { generateSamples, shuffleSamples, splitToFiles } from "./util.js";
 
-
 const margin = {top: 20, right: 10, bottom: 0, left: 10};
 const svgHeight = 400;
-
-const classes = d3.range(4);
-const samples = generateSamples([1,1,1,1]); // 22,4,15,7
-
-const circleRadius = 4;
-const circleDistance = 12;
-
 const svgWidth = d3.select('#stratification').node().getBoundingClientRect().width;
 
+const classes = d3.range(4);
+const samples = generateSamples([22,4,15,7]); // 22,4,15,7
 
+const sets = ['train', 'test'];
 let split = [0.8, 0.2]
+let splitFiles = splitToFiles(split, samples.length)
+
+const circleRadius = 4;
+const rectHeight = 40;
+
 
 const g = d3.select('#stratification')
     .attr('height', svgHeight)
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-const datasetBarMargin = {top: 0, right: 50, bottom: 0, left: 50};
+const datasetBarMargin = {top: 0, right: 70, bottom: 0, left: 70};
 
 const dataScale = d3.scalePoint()
     .domain(samples.map(s => s.sample))
     .range([0, svgWidth - margin.left - margin.right - datasetBarMargin.right - datasetBarMargin.left])
     .padding(0.5);
 
-const dataAmountScale = d3.scalePoint()
-    .domain(samples.map(s => s.sample))
-    .range([0, svgWidth - margin.left - margin.right - datasetBarMargin.right - datasetBarMargin.left]);
-
 const classColors = d3.scaleOrdinal()
     .domain(classes)
     .range(['#66c2a5','#fc8d62','#8da0cb','#e78ac3','#a6d854','#ffd92f','#e5c494','#b3b3b3']);
 
-const sets = ['train', 'test'];
 const colors = d3.scaleOrdinal()
         .domain(sets)
         .range(['#fdae61', '#2b83ba', '#abdda4']);
 
-let setXScale = d3.scaleOrdinal()
+let setXOffsetScale = d3.scaleOrdinal()
         .domain(sets)
         .range([0, (svgWidth - margin.right - margin.left) * split[0] + datasetBarMargin.right + datasetBarMargin.left]);
-
-const rectHeight = 40;
 
 g.append('rect')
     .attr('transform', `translate(${datasetBarMargin.left}, ${datasetBarMargin.top})`)
@@ -60,10 +53,6 @@ let setG = g.append('g')
     .attr('transform', `translate(0, ${100})`);
 
 let drawSetRects = function() {
-    let nrFiles = splitToFiles(split, samples.length)
-
-    setXScale.range([0, dataScale(nrFiles[0] - 1) + dataScale.step() / 2 + datasetBarMargin.right + datasetBarMargin.left]);
-
     setG.selectAll('.set-rect')
         .data(sets)
         .join(
@@ -72,32 +61,17 @@ let drawSetRects = function() {
                 .attr('y', 0)
                 .attr('height', rectHeight)
                 .attr('fill', d => colors(d))
-                .attr('opacity', 0.3),
+                .attr('opacity', 0.3)
+                .attr('x', (d, i) => setXOffsetScale(d))
+                .attr('width', (_, i) => splitFiles[0] === 0 ? 0 : dataScale(splitFiles[i] - 1) + dataScale.step() / 2),
             update => update.transition()
-                .duration(5000)
-        ).attr('x', (d, i) => {
-            if (i === 0) {
-                return 0;
-            } else {
-                return setXScale(d);
-            }
-        }).attr('width', (_, i) => {
-            if (nrFiles[i] === 0) {
-                return 0;
-            } else {
-                let step;
-                if (nrFiles[i] === samples.length) {
-                    step = dataScale.step();
-                } else {
-                    step = dataScale.step() / 2;
-                }
-
-                return dataScale(nrFiles[i] - 1) + step;
-            }
-        });
+                .duration(1000)
+                .attr('x', (d, i) => setXOffsetScale(d))
+                .attr('width', (_, i) => splitFiles[0] === 0 ? 0 : dataScale(splitFiles[i] - 1) + dataScale.step() / 2),
+        )
 }
 
-drawSetRects()
+drawSetRects();
 
 let clearSamples = function() {
     samples.forEach(s => s.set = null);
@@ -157,36 +131,6 @@ const barHeight = 50;
 
 const labelMargin = {top: 0, right: 0, bottom: 0, left: 0};
 
-// g.selectAll('.set-label')
-//     .data(sets)
-//     .join(
-//         enter => enter.append('text')
-//             .attr('class', 'set-label')
-//             .attr('x', d => setScale(d) + setScale.bandwidth() / 2)
-//             .attr('y', svgHeight - margin.top - margin.bottom - labelMargin.bottom)
-//             .attr('text-anchor', 'middle')
-//             .attr('font-style', 'italic')
-//             .attr('font-size', '11pt')
-//             .html(d => 'Train')
-//     );
-
-// const textHeight = d3.select('.set-label').node().getBBox().height;
-
-// const lineWidth = 1;
-
-// g.selectAll('.set-line')
-//     .data(sets)
-//     .join(
-//         enter => enter.append('line')
-//             .attr('class', 'set-line')
-//             .attr('x1', d => setScale(d))
-//             .attr('x2', d => setScale(d) + setScale.bandwidth())
-//             .attr('y1', svgHeight - margin.top - margin.bottom - labelMargin.bottom - textHeight - labelMargin.top)
-//             .attr('y2', svgHeight - margin.top - margin.bottom - labelMargin.bottom - textHeight - labelMargin.top)
-//             .attr('stroke', 'black')
-//             .attr('stroke-width', lineWidth)
-//     );
-
 const withoutShuffling = function() {
     clearSamples();
     initSamples();
@@ -241,7 +185,12 @@ d3.select('#split-btn').on('click', (e) => {
 d3.select("#ratio-slider").on("input", (e) => {
     let trainTestRatio = d3.select(e.currentTarget).property('value');
     d3.select('#train-ratio').html(trainTestRatio);
+
+    // update data
     split = [trainTestRatio / 100, 1 - (trainTestRatio / 100)];
-    drawSetRects()
+    splitFiles = splitToFiles(split, samples.length);
+    setXOffsetScale.range([0, (dataScale(splitFiles[0] - 1) + dataScale.step() / 2 || 0) + datasetBarMargin.right + datasetBarMargin.left]);
+
+    drawSetRects();
 });
 
