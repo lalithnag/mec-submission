6e6cd422eeb5acae5334225e8483bcd60e7814d2
Georgii Kostiuchik <georgii.kostiuchik@gmail.com>
2023-08-21 02:33:50 +0200

    fix generation of groups

--- docs/util.js
+++ docs/util.js
@@ -1,16 +1,27 @@
 function generateSamples(nrSamples) {
-    let classIndex = nrSamples.flatMap((e,i) => d3.range(e).map(_ => i));
+    const classIndex = nrSamples.flatMap((e,i) => d3.range(e).map(_ => i));
+
+    const nrGroup1Samples = 9;
+    let groupIdx = []
+
+    while(groupIdx.length < nrGroup1Samples) {
+        let index = Math.floor(Math.random() * d3.sum(nrSamples));
+
+        if(!groupIdx.includes(index)) {
+            groupIdx.push(index)
+        }
+    }
+
+    let groupIndex = classIndex.map((_,i) => groupIdx.includes(i) ? 1 : 0);
 
     return d3.range(d3.sum(nrSamples)).map((_,i) => {
-        let group = Math.random() >= 0.2 ? 0 : 1;
-        return {sample: i, dataclass: classIndex[i], sortIdx: i, displayIdx: i, set: null, group: group}
+        return {sample: i, dataclass: classIndex[i], sortIdx: i, displayIdx: i, set: null, group: groupIndex[i]}
     })
 }
 
 // inplace shuffling
 function shuffleSamples(array) {
     let j, x, i;
-    debugger
 
     for (i = array.length - 1; i > 0; i--) {
         j = Math.floor(Math.random() * (i + 1));
